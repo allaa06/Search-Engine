677f20537611341b14b6bd7d124b36bf8b72bb6f
allaa06 <138693563+allaa06@users.noreply.github.com>
2024-02-23 20:06:22 +0400

    Update file1.js

--- Front/Day1/task2/file1.js
+++ Front/Day1/task2/file1.js
@@ -1,1 +1,11 @@
+function myFunction()
+{
+    var button = document.getElementById("myButton");
+    if(button.textContent === "Click Me")
+    {
+        button.textContent = "Clicked";
+    } else {
+        button.textContent = "Click Me";
+    }
 
+}
